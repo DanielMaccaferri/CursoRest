a49eb0b890301aae227951aa5dc098ee2efc9b69
danielmaccaferri <danmaccaferri@hotmail.com>
2021-08-09 08:06:08 -0300

    second commit

--- src/main/java/br/ce/maccaferri/rest/UserJsonTest.java
+++ src/main/java/br/ce/maccaferri/rest/UserJsonTest.java
@@ -2,17 +2,20 @@ package br.ce.maccaferri.rest;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.contains;
-
-
-import java.util.Arrays;
-
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.lessThan;
+
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -118,4 +121,39 @@ public class UserJsonTest {
 		.body("salary", contains(1234.5678f, 2500, null)) // tem que mandar todos os valortes da lista e na ordem correta
 		;		
 	}	
-}
+	
+	@Test
+	public void devofazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+		.statusCode(200)	
+		.body("$", hasSize(3)) //$ procurando na raiz poderia também deixar em branco ""
+		//.body("", hasSize(3))
+		.body("age.findAll{it <= 25}.size()", is(2))
+		.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+		.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))//lista
+		.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))//transforma a lista em objeto //[0] é o primeiro elemento da lista
+		.body("findAll{it.age <= 25}[1].name", is("Ana Júlia"))//transforma a lista em objeto //[0] é o primeiro elemento da lista
+		.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))//transforma a lista em objeto //[0] é o primeiro elemento da lista
+		.body("find{it.age <= 25}.name", is("Maria Joaquina"))//find pega o primeiro elemento
+		.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+		//.body("findAll{it.name.lenght() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+		.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+		.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem ("MARIA JOAQUINA"))
+		.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+		.body("age.collect{it * 2}", hasItems (60 ,50 ,40))
+		.body("id.max()", is (3))
+		.body("id.min()", is (1))
+		.body("salary.min()", is (1234.5678f))
+		.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+		.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(4000d)))
+
+
+		;
+		
+		
+		}	
+	}
+
